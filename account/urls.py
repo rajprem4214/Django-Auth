5b3bd3596868abfe14c8b02162437a5ad576e7b1
rajprem4214 <rajprem4214@gmail.com>
2022-10-19 00:17:57 +0530

    first commit

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from account.views import UserRegistrationView, UserChangePasswordView, UserLoginView, UserProfileView
+ 
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+]
